11d94aab29778b928581ea866adcc06a07f15a17
Marcus Nyeholt <marcus@silverstripe.com.au>
2016-10-12 11:06:08 +1100

    FIX Added 'disable' flag to be able to disable overall tracking from config

--- code/services/DataChangeTrackService.php
+++ code/services/DataChangeTrackService.php
@@ -6,8 +6,14 @@
 class DataChangeTrackService {
 
 	protected $dcr_cache = array();
+    
+    public $disabled = false;
 
 	public function track(DataObject $object, $type = 'Change') {
+
+        if ($this->disabled) {
+            return;
+        }
 
 		if (!isset($this->dcr_cache["{$object->ID}-{$object->Classname}"])) {
 			$this->dcr_cache["{$object->ID}-{$object->Classname}"] = DataChangeRecord::create();
